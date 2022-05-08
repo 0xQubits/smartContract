5601b960015b1482f855470ed638876f756aa79a
Lanre Ojetokun <ojetoks@gmail.com>
2022-05-08 20:56:43 +0100

    rename variables

--- contracts/BitToken.sol
+++ contracts/BitToken.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.11;
 
-// For BitToken
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -13,39 +12,40 @@ import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 contract BitToken is ERC721,IERC721Receiver, AccessControl {
     using Counters for Counters.Counter;
 
-    uint256 public constant DECIMAL = 12;
-    uint256 public constant TOTAL = uint256(10)**uint256(DECIMAL);
+    uint public constant DECIMAL = uint(12);
+    uint public constant TOTAL = uint(10)**DECIMAL;
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     Counters.Counter private _tokenIdCounter;
 
 
     
-    struct OriginalToken {
+    
+    struct Token {
+        address owner;
+        uint portion;
+        bool hasBeenAltered;
+        bytes32 externalTokenHash;
+
+    }
+    Token[] public TokenArr;
+
+    struct ExternalToken {
         address sender;
         address contract_;
-        uint256 tokenId;
-
- 
-
-    }
-    struct DividedToken {
-        address owner;
-        uint256 portion;
-        bool has_been_altered;
-        bytes32 original_token_hash;
+        uint tokenId;
+        uint[] historyArr;
+        uint[] activeTokenIdsArr;
 
     }
-    //          unique_hash
-    mapping(bytes32 => OriginalToken) public OriginalTokens;
-    DividedToken[] public DividedTokens;
+    mapping(bytes32 => ExternalToken) public ExternalTokenMap;
+
+    event ReceivedExternalToken(address sender ,address operator, address from,uint tokenId,bytes32 hash_value );
+    event OwnershipModified(address from ,address to, uint externalTokenId,uint portion );
 
 
     
 
-    event ReceivedTokenEvent(address sender ,address operator, address from,uint256 tokenId,bytes32 hash_value );
-    event DividedTokenEvent(address from ,address to, uint256 tokenId );
-    /// @custom:oz-upgrades-unsafe-allow constructor
-
+    
     constructor() ERC721("MyToken", "MTK") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
@@ -53,72 +53,88 @@ contract BitToken is ERC721,IERC721Receiver, AccessControl {
 
 
 
-    // function safeMintDividedToken(address from,address to) public onlyRole(MINTER_ROLE) {
-    function safeMintDividedToken(
+    function safeMintToken(
             address from,
             address to,
-            uint256 portion,
-            bytes32 original_token_hash
-        ) private {
+            uint portion,
+            bytes32 externalTokenHash
+        ) private returns (uint){
         
 
-        uint256 tokenId = _tokenIdCounter.current();
-        
-        DividedToken memory token;
+        Token memory token;
         token.owner = to;
         token.portion = portion;
-        token.original_token_hash = original_token_hash;
-        DividedTokens.push(token);
-        emit DividedTokenEvent(from,to,tokenId);
-
-
+        token.externalTokenHash = externalTokenHash;
+        TokenArr.push(token);
+        
+        uint tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
+        ExternalToken storage externalToken;
+        externalToken = ExternalTokenMap[externalTokenHash];
+        externalToken.historyArr.push(tokenId);
+
+        emit OwnershipModified(
+            from,
+            to,
+            externalToken.tokenId,
+            portion
+        );
+        return tokenId;
+
     }
 
     
-    function hash_func(
+    function makeExternalTokenHash(
         address _contract_address,
-        uint256 _tokenId
-    ) private pure returns (bytes32) {
+        uint _tokenId
+    ) public pure returns (bytes32) {
         return keccak256(abi.encodePacked(_contract_address, _tokenId));
     }
 
 
 
-    function divideToken(
-        uint256 _tokenId,
+    function modifyTokenOwnership(
+        uint _tokenId,
         address[] memory _new_owners, 
-        uint256[] memory _new_owners_portion // in 1,000,000,000,000
-        // bool[] memory _is_transferable
+        uint[] memory _new_owners_portion
 
         )  public {
             require(_new_owners.length == _new_owners_portion.length);
 
-            DividedToken storage token = DividedTokens[_tokenId];
+            Token storage token = TokenArr[_tokenId];
             require(token.owner != address(0));
             require(token.owner == msg.sender,"Only the owner may transfer this NFT");
-            require(token.has_been_altered == false,"Token may not  be altered more than once");
+            require(token.hasBeenAltered == false,"Token may not  be altered more than once");
         
 
-            uint256 total = 0;
-            for (uint256 i=0;i < _new_owners.length;i++){
+            uint total = 0;
+            for (uint i=0;i < _new_owners.length;i++){
                 require(_new_owners_portion[i] <= token.portion,"You can't transfer more than 100% of your holding");
                 total = total + _new_owners_portion[i];
             }
             require(total == token.portion,"Incorrect portion allocation. They sum up to more or less than 100%");
 
-            token.has_been_altered = true;
+            token.hasBeenAltered = true;
             
+            uint[] memory new_divided_token_ids = new uint[](_new_owners.length);
 
-            for (uint256 i=0;i < _new_owners.length;i++){
-                safeMintDividedToken(
-                    token.owner,
+
+            for (uint i=0;i < _new_owners.length;i++){
+                uint new_token_id;
+                new_token_id =safeMintToken(
+                      token.owner,
                      _new_owners[i],
                      _new_owners_portion[i],
-                     token.original_token_hash
-                     );
+                     token.externalTokenHash
+                );
+                new_divided_token_ids[i] = new_token_id;
             }
+            updateActiveTokenArr(
+                token.externalTokenHash,
+                _tokenId,
+                new_divided_token_ids
+            );
 
             
         
@@ -126,93 +142,154 @@ contract BitToken is ERC721,IERC721Receiver, AccessControl {
 
 
 
-    function getOriginalToken(
+    function getExternalToken(
         address _contract,
-        uint256 _tokenId
-    ) public view returns (OriginalToken memory){
-        bytes32 hash_value = hash_func(_contract, _tokenId);
-        return OriginalTokens[hash_value];
+        uint _tokenId
+    ) public view returns (ExternalToken memory){
+        bytes32 hash_value = makeExternalTokenHash(_contract, _tokenId);
+        return ExternalTokenMap[hash_value];
     }
 
-    function getDividedToken(
-        uint256 _tokenId
-    ) public view returns (DividedToken memory){
-        return DividedTokens[_tokenId];
+
+
+
+    function getToken(
+        uint _tokenId
+    ) public view returns (Token memory){
+        return TokenArr[_tokenId];
+    }
+
+
+
+
+    function getTokenArr(
+        uint[] memory _tokenIds
+    ) public view returns (Token[] memory){
+        
+        Token[] memory tokens = new Token[](_tokenIds.length);
+        for (uint i=0;i<_tokenIds.length;i++){
+            tokens[i] = TokenArr[_tokenIds[i]];
+        }
+        return tokens;
     }
     
 
-    function onERC721Received(
-        address operator,
-        address from,
-        uint256 tokenId,
-        bytes memory
-    ) public virtual override returns (bytes4) {
-
-    // check that token has not previoudly been assigned
-        OriginalToken memory token;
-        token.contract_ = msg.sender;
-        token.sender = from;
-        token.tokenId = tokenId;
-
-        bytes32 hash_value = hash_func(msg.sender, tokenId);
-        OriginalTokens[hash_value] = token;
-        emit ReceivedTokenEvent(msg.sender,operator,from, tokenId,hash_value);
-
-        safeMintDividedToken(
-                     token.sender,
-                     token.sender,
-                     TOTAL,
-                     hash_value
-                     );
-
-
-        return this.onERC721Received.selector;
+     function getActiveTokenIds(
+        bytes32 externalTokenHash
+    ) public view returns (uint[] memory) {
+        ExternalToken memory externalToken = ExternalTokenMap[externalTokenHash]; 
+        return externalToken.activeTokenIdsArr;
     }
+
+    function getActiveTokens(
+        bytes32 externalTokenHash
+    ) public view returns (Token[] memory) {
+        ExternalToken memory externalToken = ExternalTokenMap[externalTokenHash];
+        uint[] memory activeTokenIdsArr = externalToken.activeTokenIdsArr;
+
+        Token[] memory tokens = new Token[](activeTokenIdsArr.length);
+        for (uint i=0;i<activeTokenIdsArr.length;i++){
+            tokens[i] = TokenArr[activeTokenIdsArr[i]];
+        }
+        return tokens;
+    }
+
+    
+
+
+    function updateActiveTokenArr(
+        bytes32 externalTokenHash,
+        uint outgoingTokenId,
+        uint[] memory incomingTokenIds
+    ) private {
+        require(incomingTokenIds.length > 0,"Incoming Token Ids array must have at least one value");
+        
+        ExternalToken storage externalToken = ExternalTokenMap[externalTokenHash];
+        uint[] storage activeTokenIdsArr = externalToken.activeTokenIdsArr;
+        for (uint i=0; i < activeTokenIdsArr.length;i++){
+            uint tokenId = activeTokenIdsArr[i];
+            if (tokenId == outgoingTokenId){
+                delete activeTokenIdsArr[i];
+                for (uint j=0;j<incomingTokenIds.length;j++){
+                    if (j==0){
+                        activeTokenIdsArr[i] = incomingTokenIds[j];
+                    } else {
+                        activeTokenIdsArr.push(incomingTokenIds[j]);
+                    }
+                }
+            }
+        }
+    }
+
+    
 
     
 
     function returnToken(
-        uint256[] memory _tokenIds
+        bytes32 externalTokenHash
 
     ) public returns (bool) {
         
-        uint256 required_total = TOTAL;
 
-        uint256 total = 0;
-        bytes32 last_original_token_hash;
-        for (uint256 i=0;i < _tokenIds.length;i++){
-            uint256 DTokenId = _tokenIds[i];
-            DividedToken storage DToken = DividedTokens[DTokenId];
-            require(DToken.owner != address(0));
-            require(DToken.owner == msg.sender,"Only the owner may transfer this NFT");
-            require(DToken.has_been_altered == false,"One token has already been altered ");
-
-            if (i > 0 ){
-                require(DToken.original_token_hash == last_original_token_hash,"Tokens sent point to different contract or token");
-            } else {
-                last_original_token_hash = DToken.original_token_hash;
-            }
-            total = total + DToken.portion;
+        uint expected_total = 0;
+        ExternalToken storage externalToken;
+        externalToken =  ExternalTokenMap[externalTokenHash];
+        uint[] storage activeTokenIdsArr = externalToken.activeTokenIdsArr;
+        
+        for (uint i=0;i < activeTokenIdsArr.length;i++){
+            uint tokenId = activeTokenIdsArr[i];
+            Token storage token = TokenArr[tokenId];
+            require(token.owner != address(0));
+            require(token.owner == msg.sender,"Only the owner may transfer this NFT");
+            require(token.hasBeenAltered == false,"One token has already been altered ");
+            expected_total += token.portion;
         }
-        require(total==required_total,"You must have 100% ownership of the NFT in order to perform this function");
-        
-        
-        OriginalToken storage OToken = OriginalTokens[last_original_token_hash];
-    
+        require(expected_total==TOTAL,"You must have 100% ownership of the NFT in order to perform this function");
 
-        ERC721 nft = ERC721(OToken.contract_);
-        nft.transferFrom(
-                address(this),
-                msg.sender,
-                OToken.tokenId
-            );
+        ERC721 nft = ERC721(externalToken.contract_);
+        nft.transferFrom(address(this),msg.sender,externalToken.tokenId);
 
         // make sure they they can't be used to send back token twice 
-        for (uint256 i=0;i < _tokenIds.length;i++){
-            DividedToken storage DToken = DividedTokens[_tokenIds[i]];
-            DToken.has_been_altered = true;            
+        for (uint i=0;i < activeTokenIdsArr.length;i++){
+            Token storage token = TokenArr[activeTokenIdsArr[i]];
+            token.hasBeenAltered = true;            
         }
         return true;
+    }
+
+
+
+    function onERC721Received(
+        address operator,
+        address from,
+        uint tokenId,
+        bytes memory
+    ) public virtual override returns (bytes4) {
+
+    // check that token has not previoudly been assigned
+        ExternalToken memory externalToken;
+        externalToken.contract_ = msg.sender;
+        externalToken.sender = from;
+        externalToken.tokenId = tokenId;
+        bytes32 hash_value = makeExternalTokenHash(msg.sender, tokenId);
+
+        uint[] memory new_divided_token_ids = new uint[](1);
+        uint new_divided_token_id;
+        new_divided_token_id = safeMintToken(
+            externalToken.sender,
+            externalToken.sender,
+            TOTAL,
+            hash_value
+            );
+        
+        new_divided_token_ids[0] = new_divided_token_id;
+        externalToken.activeTokenIdsArr = new_divided_token_ids;
+
+
+        ExternalTokenMap[hash_value] = externalToken;
+        emit ReceivedExternalToken(msg.sender,operator,from, tokenId,hash_value);
+
+        return this.onERC721Received.selector;
     }
 
 
--- test/BitToken.js
+++ test/BitToken.js
@@ -1,3 +1,6 @@
+// This document does not not showcase my 
+// system design, modularity or abstraction skills
+// I'm just testing stuff out
 const { expect } = require("chai");
 
 let bitToken;
@@ -23,25 +26,40 @@ before(async function () {
     expect(await game.ownerOf(1)).to.equal(owner.address);
   });
 
+let testTotal  = (activeTokens)=> {
+    let total = 0
+    for (i of activeTokens){
+        total+=i.portion.toNumber()
+    }
+    expect(total).to.equal(10**12)
+}
 
 
 describe("Initialization", function () {
-  it("Should send a token representing 100% ownership to the NFT sender when it receives an NFT", async function () {
-    // Ensure that the NFT can be sent to the BitToken contract
-    // and 100% ownership is originally assigned to the sender of the NFT
-    let safeTransferTx = await game["safeTransferFrom(address,address,uint256)"](owner.address,bitToken.address,1);
-    await safeTransferTx.wait()
+    it("Should send a token representing 100% ownership to the NFT sender when it receives an NFT", async function () {
+        // Ensure that the NFT can be sent to the BitToken contract
+        // and 100% ownership is originally assigned to the sender of the NFT
+        let safeTransferTx = await game["safeTransferFrom(address,address,uint256)"](owner.address,bitToken.address,1);
+        await safeTransferTx.wait()
+
+        expect(await game.ownerOf(1)).to.equal(bitToken.address);
+
     
-    let originalToken = await bitToken.getOriginalToken(game.address,1);
-    expect(originalToken.contract_ ).to.equal(game.address);
-    expect(originalToken.sender ).to.equal(owner.address);
+        let originalToken = await bitToken.getExternalToken(game.address,1);
+        expect(originalToken.contract_ ).to.equal(game.address);
+        expect(originalToken.sender ).to.equal(owner.address);
 
 
-    let dividedToken = await bitToken.getDividedToken(0);
-    expect(dividedToken.owner).to.equal(owner.address);
-    expect(dividedToken.portion).to.equal(10**12);
-    expect(dividedToken.has_been_altered).to.equal(false);
-  });  
+        let dividedToken = await bitToken.getToken(0);
+        expect(dividedToken.owner).to.equal(owner.address);
+        expect(dividedToken.portion).to.equal(10**12);
+        expect(dividedToken.hasBeenAltered).to.equal(false);
+
+        let hash_value = await bitToken.makeExternalTokenHash(game.address,1)
+        let activeTokens = await bitToken.getActiveTokens(hash_value)
+        
+        testTotal(activeTokens)
+    });  
 });
 
 
@@ -51,7 +69,11 @@ describe("Split", function () {
     it("Should send percentage of the original token to new owners", async function () {
         // Ensure that the NFT can be further divided
         // ensure that it is sent to the intended new owners 
-        let new_owners = [owner.address,addr1.address,addr2.address,addr3.address]
+        let new_owners = [owner.address,
+            addr1.address,
+            addr2.address,
+            addr3.address
+        ]
         let new_owners_portion = [
             1 * 10 ** 11,
             3 * 10 ** 11,
@@ -59,21 +81,26 @@ describe("Split", function () {
             4 * 10 ** 11,
         ]
         
-        let divisionTx = await bitToken.divideToken(0,new_owners,new_owners_portion);
-        res = await divisionTx.wait()
-        console.log(res.events)
+        let divisionTx = await bitToken.modifyTokenOwnership(0,new_owners,new_owners_portion);
+        await divisionTx.wait()
+        // console.log(res.events)
+
         
 
-        let divided_token = await bitToken.getDividedToken(0); 
-        expect(divided_token.has_been_altered, true, "The first divided token should now be altered");
+        let divided_token = await bitToken.getToken(0); 
+        expect(divided_token.hasBeenAltered, true, "The first divided token should now be altered");
 
         for (i=1; i < 5 ;i++){
-            let new_divided_token = await bitToken.getDividedToken(i);
+            let new_divided_token = await bitToken.getToken(i);
             expect(new_divided_token.owner).to.equal(new_owners[i-1])
             expect(new_divided_token.portion).to.equal(new_owners_portion[i-1]);
-            expect(new_divided_token.has_been_altered).to.equal(false);
+            expect(new_divided_token.hasBeenAltered).to.equal(false);
 
         }
+        let hash_value = await bitToken.makeExternalTokenHash(game.address,1)
+        let activeTokens = await bitToken.getActiveTokens(hash_value)
+        testTotal(activeTokens)
+
     });
 
 
@@ -87,21 +114,25 @@ describe("Split", function () {
         let new_owners = [owner.address,addr1.address]
         let new_owners_portion = [5 * 10 ** 10,5 * 10 ** 10]
         owners_token_id = 1
-        let divisionTx = await bitToken.divideToken(owners_token_id,new_owners,new_owners_portion);
+        let divisionTx = await bitToken.modifyTokenOwnership(owners_token_id,new_owners,new_owners_portion);
         await divisionTx.wait()
 
-
-        let divided_token = await bitToken.getDividedToken(owners_token_id); 
-        expect(divided_token.has_been_altered, true, "The first divided token should now be altered");
+        let divided_token = await bitToken.getToken(owners_token_id); 
+        expect(divided_token.hasBeenAltered, true, "The first divided token should now be altered");
 
         for (i=5; i < 7 ;i++){
-            let new_divided_token = await bitToken.getDividedToken(i);
+            let new_divided_token = await bitToken.getToken(i);
             expect(new_divided_token.owner).to.equal(new_owners[i-5])
             expect(new_divided_token.portion).to.equal(new_owners_portion[i-5]);
-            expect(new_divided_token.has_been_altered).to.equal(false);
+            expect(new_divided_token.hasBeenAltered).to.equal(false);
 
         }
+        let hash_value = await bitToken.makeExternalTokenHash(game.address,1)
+        let activeTokens = await bitToken.getActiveTokens(hash_value)
+        testTotal(activeTokens)
+
     });
+
 
 
   });
